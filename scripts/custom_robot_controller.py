bd4b297262c4c704a1a05bb57a77ebd25d0f3f8a
Vitor Ventuzelos <skullventuzelos@hotmail.com>
2022-06-26 13:53:23 +0100

    Update custom_robot_controller.py

--- scripts/custom_robot_controller.py
+++ scripts/custom_robot_controller.py
@@ -21,8 +21,37 @@ class CustomRobotControllerNode(Node):
         # For more information on the data contained in a LaserScan message, consult http://docs.ros.org/en/api/sensor_msgs/html/msg/LaserScan.html
         #
         # The following are used to set the linear and angular velocities of the robot
-        # twist_msg.linear.x = 0.0;
-        # twist_msg.angular.z = 0.0;
+        # twist_msg.linear.x = 0.0
+        # twist_msg.angular.z = 0.0
+        ########################################
+        
+        ########################################
+        ## Example 1: A robot that moves forward
+        #twist_msg.linear.x = 1 # A positive lenear velocity makes the robot move forward
+        #twist_msg.angular.z = 0 # Zero angular velocitiy prevents the robot from turning/rotating
+        ########################################
+        
+        ########################################
+        ## Example 2: A robot that moves in a circle
+        #twist_msg.linear.x = 1 # A positive lenear velocity makes the robot move forward
+        #twist_msg.angular.z = -1 # A negative angular velocitiy makes the robot turn clockwise
+        ########################################
+        
+        ########################################
+        ## Example 3: A robot that moves in circles, and turns around itself when it finds an obstacle in front of it
+        ## Find if the laser in front of the robot is detecting an obstacle too close to them
+        #num_lasers = lidar_scan_msg.ranges.size()
+        #max_distance = 1.00 # maximum allowed distance for an obstacle in one of three sensor readings before the robot starts spining around itself
+        #middle_laser_range = lidar_scan_msg.ranges[num_lasers / 2]
+        #if( not (middle_laser_range >= lidar_scan_msg.range_min and middle_laser_range <= lidar_scan_msg.range_max) # if the distance reading is invalid or...
+        #        or middle_laser_range < max_distance): # ... the distance is below the maximum allowed distance
+        #   # ROTATE
+        #    twist_msg.linear.x = 0
+        #    twist_msg.angular.z = -2
+        #else:
+        #   # MOVE FORWARD
+        #    twist_msg.linear.x = 2;
+        #    twist_msg.angular.z = -2;
         ########################################
 
         self.twist_pub.publish(twist_msg)
